eabd2e69978005a19bd836844692ca5a635aa185
moudy99 <bobmoudy1@gmail.com>
2024-04-06 21:09:45 +0200

    .

---
+++ CLIQ_UE/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CLIQ_UE.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string UserImage { get; set; }
+        public string CoverImage { get; set; }
+    }
+}
