e50de4912293b76aeed892b67d8d7091c1a956b0
3bda1137 <abdallahmahfouz111@gmail.com>
2024-04-09 11:41:32 +0200

    update

--- CLIQ_UE/Models/Followers.cs
+++ CLIQ_UE/Models/Followers.cs
@@ -3,7 +3,7 @@
     public class Followers
     {
         public int Id { get; set; }
-        public string CurrentUser { get; set; }
+        public string FollowerId { get; set; }
         public string FollowingId { get; set; }
         public string FollowingName { get; set; }
         public string ImageUrl { get; set; }
