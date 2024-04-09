423f342acd3f34ed7bcf72763cbb3b168cca6a10
Mousa <54138093+Mousa-Ahmed98@users.noreply.github.com>
2024-04-09 14:35:42 +0200

    You can like or dislike post now

---
+++ CLIQ_UE/Services/IUserLikePostService.cs
@@ -0,0 +1,12 @@
+﻿using CLIQ_UE.Models;
+
+namespace CLIQ_UE.Services
+{
+    public interface IUserLikePostService
+    {
+        UserLikePost? Get(int postId, string UID);
+        UserLikePost Update(UserLikePost user);
+        UserLikePost Add(UserLikePost user);
+        void Delete(UserLikePost likePost);
+    }
+}
