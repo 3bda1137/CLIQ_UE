0ea4960ec1c90d9d702c54f8e9654121d91284bb
moudy99 <bobmoudy1@gmail.com>
2024-04-18 04:34:10 +0200

    update

--- CLIQ_UE/ViewModels/HomePageViewModel.cs
+++ CLIQ_UE/ViewModels/HomePageViewModel.cs
@@ -7,7 +7,11 @@ namespace CLIQ_UE.ViewModels
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
+
         public string UserImage { get; set; }
+        public string coverImage { get; set; }
+        public int numberOfFollowing { get; set; }
+
         public List<Post>? LatestPosts { get; set; }
         public string userId { get; set; }
         ApplicationUser user { get; set; }
